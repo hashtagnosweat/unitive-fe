8442c87e0939e5c3ca361fbd0de139b712e055c6
hashtagnosweat <ridorachmatan@gmail.com>
2024-01-24 12:28:15 +0700

    Added showcase item on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import { useSelector } from 'react-redux';
 import './Home.css';
 import imageHero from '../assets/home-bg.png';
 import logo from '../assets/logo.png';
+import mobileShowcase from '../assets/mobile-showcase.png';
 
 function Home() {
   const user = useSelector((state) => state.user);
@@ -29,7 +30,10 @@ function Home() {
                 experiences.
               </p>
               <Link to={user ? '/chat' : '/signup'}>
-                <button type="button" class="btn btn-primary">
+                <button
+                  type="button"
+                  className="cta-btn shadow shadow-2 btn btn-primary"
+                >
                   {user ? 'Start Learning' : 'Get Started'}
                   <i className="fas fa-comments home-message-icon"></i>
                 </button>
@@ -42,15 +46,39 @@ function Home() {
         </div>
       </div>
 
+      <div className="showcase-item-container">
+        <div className="showcase-item">
+          <div className="row">
+            <div className="showcase-left col-lg-6 text-center">
+              <img src={mobileShowcase} alt="mobile-showcase" height={500} />
+            </div>
+            <div className="d-flex flex-column justify-content-center col-lg-6">
+              <h1 className="showcase-title">Learn by Chatting</h1>
+              <p className="pt-3 showcase-text">
+                The best way to learn a language is to actually use it every
+                day! Unitive connects you with native speakers to chat with for
+                free. But this isn't your standard social app. The interface is
+                packed with innovative tools to make it fun and effortless to
+                learn a new language. You can chat with individual members, or
+                join language chats for a collaborative learning experience.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="border-top w-100 footer-container">
         <div className="footer-wrapper">
-          <div className="d-flex justify-content-center align-items-center pb-2 copyright-footer">
-            © Unitive 2024, All rights reserved.
+          <div className="icon-contact">
+            <img src={logo} style={{ height: 40 }}></img>
+            <div className="copyright-footer">
+              © Unitive 2024, All rights reserved.
+            </div>
           </div>
-          <div className="text-center text-footer">
-            Embark on a linguistic journey with Unitive! Unitive is committed to
-            providing you with a better, faster, and safer experience as you
-            traverse the exciting landscape of language acquisition.
+
+          <div className="d-flex justify-content-start text-footer">
+            Discover the world through language with Unitive – Immerse yourself
+            in a community of native speakers from around the globe.
           </div>
         </div>
       </div>
